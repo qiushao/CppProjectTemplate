a4f869bed8dd105483ea2eb4715191365d6fdc1c
qiushao <qiushaox@gmail.com>
2025-03-10 23:39:26 +0800

    fixed ci error: Unable to locate package libstdc++-*-dev on ubuntu 24.04

--- scripts/ci_setup_clang.sh
+++ scripts/ci_setup_clang.sh
@@ -5,7 +5,7 @@ set -ex
 VERSION=$1
 
 apt-get update
-apt-get install -y libc++-${VERSION}-dev libc++abi-${VERSION}-dev libstdc++-${VERSION}-dev
+apt-get install -y libc++-${VERSION}-dev libc++abi-${VERSION}-dev
 
 if [[ "${VERSION}" -ge 12 ]]; then
     apt-get install -y --no-install-recommends libunwind-${VERSION}-dev
